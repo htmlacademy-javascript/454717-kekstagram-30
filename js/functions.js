8fa807d308ade83a234085b19fad44a96dc3e012
YuliaPortnova <yuyul@yandex.ru>
2023-10-09 21:06:20 +0300

    feat: добавляет функцию поиска цифр в строке

--- js/functions.js
+++ js/functions.js
@@ -19,3 +19,24 @@ const isPalindrom = (string) => {
 };
 
 isPalindrom('Топ от');
+
+
+const parseDigits = (data) => {
+  const string = String(data);
+  let result = '';
+  for (let i = 0; i < string.length; i++) {
+    if (string[i] >= '0' && string[i] <= '9') {
+      result += string[i];
+    }
+  }
+  return (result === '') ? NaN : Number(result);
+};
+
+parseDigits('2023 год');
+parseDigits('ECMAScript 2022');
+parseDigits('-1 кефир, 0.5 батона');
+parseDigits('агент 007');
+parseDigits('а я томат');
+parseDigits(2023);
+parseDigits(-1);
+parseDigits(1.5);
