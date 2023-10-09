83725d2bdd771f196e0557f376e2c3913ed112cf
YuliaPortnova <yuyul@yandex.ru>
2023-10-09 12:34:13 +0300

    feat: добавляет функцию проверки палиндрома

--- js/functions.js
+++ js/functions.js
@@ -6,3 +6,16 @@ isShorterThanMaxLength('строка', 20); // true
 isShorterThanMaxLength('проверяемая строка', 18); // true
 // Строка длиннее 10 символов
 isShorterThanMaxLength('проверяемая строка проверяемая строка', 10); // false
+
+
+const isPalindrom = (string) => {
+  const stringToCheck = string.replaceAll(' ', '').toLowerCase();
+  for (let i = 0; i < stringToCheck.length / 2; i++) {
+    if (stringToCheck.at(i) !== stringToCheck.at(-i - 1)) {
+      return false;
+    }
+  }
+  return true;
+};
+
+isPalindrom('Топ от');
