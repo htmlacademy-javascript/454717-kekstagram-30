d9d9fa006b5ebfbeb8fee2399b326533ea5ad0a0
YuliaPortnova <yuyul@yandex.ru>
2023-10-16 11:24:45 +0300

    feat: завершает конструктор фото

--- js/main.js
+++ js/main.js
@@ -1,11 +1,23 @@
-const getRandomInteger = (min, max) => {
-  const fraction = Math.random() * (max - min + 1) + min;
-  return Math.floor(fraction);
-};
+const getRandomInteger = (min, max) => Math.floor(Math.random() * (max - min + 1) + min);
 
-const getRandomItem = (items) =>{
-  const index = Math.floor(Math.random() * items.length);
-  return items[index];
+const getRandomItem = (items) => items[getRandomInteger(0, items.length - 1)];
+
+const createCommentsData = (length) => {
+  const messages = [
+    'Всё отлично!',
+    'В целом всё неплохо. Но не всё.',
+    'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
+    'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+    'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+    'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!',
+  ];
+  const names = ['Сергей', 'Ольга', 'Анна', 'Владимир', 'Николай', 'Артём', 'Виктор', 'Наталья', 'Надежда'];
+  return new Array (length).fill(1).map((start, index) => ({
+    id: start + index,
+    avatar: `img/avatar-${getRandomInteger(1, 6)}.svg`,
+    message: getRandomItem(messages),
+    name: getRandomItem(names),
+  }));
 };
 
 const createPicturesData = (itemCount = 25) => {
@@ -15,9 +27,10 @@ const createPicturesData = (itemCount = 25) => {
     url: `photos/${start + index}.jpg`,
     description: getRandomItem(descriptions),
     likes: getRandomInteger(15, 200),
-    comments: [], //TODO: createCommentsData
+    comments: createCommentsData(getRandomInteger(0, 30)),
   }));
 };
 
 const photos = createPicturesData();
 console.table(photos);
+console.table(photos[3].comments);
