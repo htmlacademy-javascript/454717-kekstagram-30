2f63c66771c3d813b03aa7064462605df7beb393
YuliaPortnova <yuyul@yandex.ru>
2023-10-25 12:29:26 +0300

    refactor: изменяет структуру функции для отображения миниатюр
    Вносит изменения после видеовстречи с наставником

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
 import {createPicturesData} from './data.js';
 import {renderThumbnails} from './thumbnails.js';
 
-renderThumbnails(createPicturesData);
+renderThumbnails(createPicturesData());
--- js/thumbnails.js
+++ js/thumbnails.js
@@ -1,22 +1,19 @@
 const container = document.querySelector('.pictures');
-const template = document.querySelector('#picture')
-  .content
-  .querySelector('.picture');
+const template = document.querySelector('#picture');
 
-const renderThumbnails = (createData) => {
-  const thumbnails = createData();
-  const fragment = document.createDocumentFragment();
+const createThumbnails = (picturesData) => picturesData.map((properties) => {
+  const {url, description, likes, comments} = properties;
+  const thumbnail = template.content.querySelector('.picture').cloneNode(true);
+  thumbnail.querySelector('.picture__comments').textContent = comments.length;
+  thumbnail.querySelector('.picture__img').src = url;
+  thumbnail.querySelector('.picture__img').alt = description;
+  thumbnail.querySelector('.picture__likes').textContent = likes;
+  return thumbnail;
+});
 
-  thumbnails.forEach(({url, description, likes, comments}) => {
-    const thumbnail = template.cloneNode(true);
-    thumbnail.querySelector('.picture__img').src = url;
-    thumbnail.querySelector('.picture__img').alt = description;
-    thumbnail.querySelector('.picture__likes').textContent = likes;
-    thumbnail.querySelector('.picture__comments').textContent = comments.length;
-    fragment.appendChild(thumbnail);
-  });
-
-  container.appendChild(fragment);
+const renderThumbnails = (picturesData) => {
+  container.querySelectorAll('.picture').forEach((thumbnail) => thumbnail.remove());
+  container.append(...createThumbnails(picturesData));
 };
 
 export {renderThumbnails};
