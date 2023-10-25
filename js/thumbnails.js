b3f46b3d06dc2639d91e3ddeebe3c18ffa52f148
YuliaPortnova <yuyul@yandex.ru>
2023-10-25 12:35:24 +0300

    fix: меняет строчки местами

--- js/thumbnails.js
+++ js/thumbnails.js
@@ -4,10 +4,10 @@ const template = document.querySelector('#picture');
 const createThumbnails = (picturesData) => picturesData.map((properties) => {
   const {url, description, likes, comments} = properties;
   const thumbnail = template.content.querySelector('.picture').cloneNode(true);
-  thumbnail.querySelector('.picture__comments').textContent = comments.length;
   thumbnail.querySelector('.picture__img').src = url;
   thumbnail.querySelector('.picture__img').alt = description;
   thumbnail.querySelector('.picture__likes').textContent = likes;
+  thumbnail.querySelector('.picture__comments').textContent = comments.length;
   return thumbnail;
 });
 
