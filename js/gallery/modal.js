26baa01bf114f3ebce456daaea11076f37367630
YuliaPortnova <yuyul@yandex.ru>
2023-11-01 23:07:04 +0300

    feat: реализует сброс прокрутки

--- js/gallery/modal.js
+++ js/gallery/modal.js
@@ -11,15 +11,16 @@ const onDocumentKeydown = (event) => {
 
 const openModal = () => {
   modal.classList.remove('hidden');
+  modal.scroll(0, 0);
   document.body.classList.add('modal-open');
   document.addEventListener('keydown', onDocumentKeydown);
-}
+};
 
 const closeModal = () => {
   modal.classList.add('hidden');
   document.body.classList.remove('modal-open');
   document.removeEventListener('keydown', onDocumentKeydown);
-}
+};
 
 closeButton.addEventListener('click', () => closeModal());
 
